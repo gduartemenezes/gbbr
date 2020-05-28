aa1e9eda6eca1456730795a4982272dd6daf7966
Gustavo Duarte <gduartemenezes@gmail.com>
2020-05-28 09:38:55 -0300

    API-Update UserController, store res does not return sensitive data

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,9 +2,14 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const user = await User.create(req.body);
+    const { id, name, email, provider } = await User.create(req.body);
 
-    return res.json(user);
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
