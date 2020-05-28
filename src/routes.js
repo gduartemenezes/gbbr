a45b66f426eb71c9cbc0d5557f3fa25c3a10e396
Gustavo Duarte <gduartemenezes@gmail.com>
2020-05-28 13:47:00 -0300

    API- Update routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
@@ -9,6 +10,8 @@ routes.get('/', (req, res) => {
 });
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 // Auth required
 routes.put('/users', UserController.update);
