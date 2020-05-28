39ae9a33c3ce5686fafab56dedc523f5a0109dbc
Gustavo Duarte <gduartemenezes@gmail.com>
2020-05-28 12:58:20 -0300

    API- Update SessionCtrl, user validation

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,7 +3,24 @@ import User from '../models/User';
 class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
-    return res.json();
+    const user = await User.findOne({ where: { email } });
+    // E-mail validation
+
+    if (!user) {
+      return res.status(400).json({ error: 'E-mail not found' });
+    }
+    // Password validation
+    if (!(await user.checkPassword(password))) {
+      return res.status(400).json({ error: 'Password does not match.' });
+    }
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+      },
+    });
   }
 }
 
