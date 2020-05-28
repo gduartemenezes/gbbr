32d20f46eb4225627b49b059cab2da8233142e79
Gustavo Duarte <gduartemenezes@gmail.com>
2020-05-28 09:42:15 -0300

    API-Update UserController, add verification to duplitated emails

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,6 +2,14 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({
+        error: 'Email already in use, try to register with another one',
+      });
+    }
+
     const { id, name, email, provider } = await User.create(req.body);
 
     return res.json({
