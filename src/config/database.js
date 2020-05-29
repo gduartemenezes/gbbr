b0d5f9a79e75ddcc6ac012cd1fcf2969053a4ad0
Gustavo Duarte <gduartemenezes@gmail.com>
2020-05-29 14:27:10 -0300

    API- Fix dataBase config, dialect issue

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,16 @@
+// eslint-disable-next-line no-unused-vars
 const env = require('dotenv/config');
 
 module.exports = {
-  development: {
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-    },
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
   },
 };
