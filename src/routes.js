509791f629ca05e56e5b215832d2348ccda995e8
Gustavo Duarte <gduartemenezes@gmail.com>
2020-05-29 15:36:58 -0300

    API- Add ProviderCtrl.index

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,5 +23,6 @@ routes.use(authMiddleware);
 // Auth required
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
 
 export default routes;
