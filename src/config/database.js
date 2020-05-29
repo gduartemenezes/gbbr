6064a53580919adcbba44230c2757817c73c09ea
Gustavo Duarte <gduartemenezes@gmail.com>
2020-05-29 13:16:32 -0300

    Update database.config - import env variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,17 @@
+const env = require('dotenv/config');
+
 module.exports = {
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  development: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
   },
 };
