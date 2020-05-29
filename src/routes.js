d6ac28ea57100c834ab58542adfd0765ea5b9122
Gustavo Duarte <gduartemenezes@gmail.com>
2020-05-29 14:34:33 -0300

    API- Add File routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', (req, res) => {
   return res.json({ ok: true });
@@ -15,5 +21,6 @@ routes.use(authMiddleware);
 
 // Auth required
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
